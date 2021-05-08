5cee66482b1d4d967f09155f8541fd790edb73d8
Vineet Kumar Rai <vineetrai@outlook.com>
2021-05-08 15:26:31 +0530

    Final tested code

--- Instagram dp animation/ViewController.swift
+++ Instagram dp animation/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .black
+        let story = StoryDPView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        story.setupForStory(image: UIImage(named: "1")!)
+        self.view .addSubview(story)
+        story.center = self.view.center
     }
 
 
