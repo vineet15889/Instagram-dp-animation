bd00ce122b6b87d9c3fff8757314041574374138
Vineet Kumar Rai <vineetrai@outlook.com>
2022-03-15 07:13:31 +0530

    Update iOS version

--- Instagram dp animation/ViewController.swift
+++ Instagram dp animation/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
-
+import SuitabilityPredictionClient
 class ViewController: UIViewController {
-
+    let suitabilityClient: SuitabilityClient = .configure(withKey: "2671796e93c447df8a71e478940d437c")!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .black
@@ -16,6 +16,25 @@ class ViewController: UIViewController {
         story.setupForStory(image: UIImage(named: "1")!)
         self.view .addSubview(story)
         story.center = self.view.center
+        if #available(iOS 13.0.0, *) {
+            self.imagePre()
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @available(iOS 13.0.0, *)
+    func imagePre(){
+        Task {
+            do {
+                let output = try? await suitabilityClient.predict(
+                    input: UIImage(named: "1")!)
+                print(output ?? "")
+            } catch {
+                // HANDLE ERROR
+                print(error)
+            }
+        }
     }
 
 
